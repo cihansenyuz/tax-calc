194c44f252b18dc46aeddacb6aa4fef4ea2a446d
cihansenyuz <cihansenyuz@hotmail.com>
2025-07-28 09:39:22 +0300

    update cmake and main to show GUI

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include <QApplication>
+#include "../gui/mainwindow.hpp"
 
 int main(int argc, char *argv[]) {
     QApplication application(argc, argv);
+
+    MainWindow main_window;
+    main_window.show();
 
     return application.exec();
 }

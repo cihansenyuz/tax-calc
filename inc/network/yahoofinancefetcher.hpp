f9381c06276f33815e6847b17a52a3273e3be707
cihansenyuz <cihansenyuz@hotmail.com>
2025-08-05 15:26:55 +0300

    Implement YahooFinanceFetcher to use HttpManager for HTTP requests to get symbol and names of assets

---
+++ inc/network/yahoofinancefetcher.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <QObject>
+#include <QString>
+#include <QList>
+#include <QPair>
+#include "httpmanager.hpp"
+
+class YahooFinanceFetcher : public QObject {
+    Q_OBJECT
+public:
+    explicit YahooFinanceFetcher(HttpManager *http_manager, QObject *parent = nullptr);
+    void fetchSymbols(const QString &query);
+
+signals:
+    void symbolsFetched(const QList<QPair<QString, QString>> &symbols); // symbol, shortname
+    void fetchFailed(const QString &error);
+
+private slots:
+    void onJsonFetched(const std::shared_ptr<QJsonObject> &json);
+    
+private:
+    HttpManager *http_manager_;
+    static constexpr const char* apiEndPoint = "https://query1.finance.yahoo.com/v1/finance/search";
+    static constexpr const char* quotesField = "quotesCount=10";
+    static constexpr const char* newsCountField = "newsCount=0";
+};
---
+++ src/network/yahoofinancefetcher.cpp
@@ -0,0 +1,40 @@
+#include "../../inc/network/yahoofinancefetcher.hpp"
+#include <QUrl>
+#include <QUrlQuery>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QDebug>
+
+YahooFinanceFetcher::YahooFinanceFetcher(HttpManager *http_manager, QObject *parent)
+    : QObject(parent), http_manager_(http_manager) {
+    connect(http_manager_, &HttpManager::jsonFetched, this, &YahooFinanceFetcher::onJsonFetched);
+}
+
+void YahooFinanceFetcher::fetchSymbols(const QString &query) {
+    QString encodedQuery = QUrl::toPercentEncoding(query);
+    QString api_query = QString("%1?q=%2&%3&%4").arg(apiEndPoint, encodedQuery, quotesField, newsCountField);
+    http_manager_->fetchJsonData(api_query);
+}
+
+void YahooFinanceFetcher::onJsonFetched(const std::shared_ptr<QJsonObject> &json) {
+    if (!json) {
+        emit fetchFailed("JSON fetch failed or is null");
+        return;
+    }
+    QList<QPair<QString, QString>> results;
+    if (json->contains("quotes") && (*json)["quotes"].isArray()) {
+        QJsonArray quotes = (*json)["quotes"].toArray();
+        for (const QJsonValue &val : quotes) {
+            if (val.isObject()) {
+                QJsonObject quoteObj = val.toObject();
+                QString symbol = quoteObj.value("symbol").toString();
+                QString shortname = quoteObj.value("shortname").toString();
+                if (!symbol.isEmpty() && !shortname.isEmpty()) {
+                    results.append(qMakePair(symbol, shortname));
+                }
+            }
+        }
+    }
+    emit symbolsFetched(results);
+}

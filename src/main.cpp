daf7875276776600cd14ee3ef62291d9d301d8a6
cihansenyuz <cihansenyuz@hotmail.com>
2026-01-03 19:34:26 +0300

    use Logger in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <QApplication>
 #include "../gui/mainwindow.hpp"
+#include "../inc/logger.hpp"
 
 int main(int argc, char *argv[]) {
+    Logger::initialize("tax_calc.log", false);
+    
     QApplication application(argc, argv);
     application.setApplicationName("Hisse Beyan Yardımcısı");
     application.setApplicationVersion("0.1");
@@ -9,5 +12,8 @@ int main(int argc, char *argv[]) {
     MainWindow main_window;
     main_window.show();
 
-    return application.exec();
+    int result = application.exec();
+    
+    Logger::shutdown();    
+    return result;
 }
